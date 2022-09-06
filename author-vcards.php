5d9855fd2201d647c9f75089e1520a27dbf0f772
brad1066 <brad@pie.co.de>
2022-09-06 15:10:04 +0100

    Auto Update first pass

--- author-vcards.php
+++ author-vcards.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Author VCards
  * Plugin URI:  #
@@ -6,24 +7,37 @@
  * Author:      The team at PIE
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
-function makeplugins_add_vcard_endpoint() {
-    add_rewrite_endpoint( 'vcf', EP_AUTHORS );
-}
-add_action( 'init', 'makeplugins_add_vcard_endpoint' );
+/**
+ * Load Composer autoloader
+ */
+require_once get_template_directory() . '/vendor/autoload.php';
+$update_checker = Puc_v4_Factory::buildUpdateChecker(
+    'http://212.71.239.229/releases/plugins/author-vcards/release-data.json',
+    __FILE__,
+    'author-vcards'
+);
 
-function makeplugins_json_template_redirect() {
+function makeplugins_add_vcard_endpoint()
+{
+    add_rewrite_endpoint('vcf', EP_AUTHORS);
+}
+add_action('init', 'makeplugins_add_vcard_endpoint');
+
+function makeplugins_json_template_redirect()
+{
     global $wp_query;
 
     // if this is not a request for json or a singular object then bail
-    if ( ! isset( $wp_query->query_vars['vcf'] ) || ! is_author() )
+    if (! isset($wp_query->query_vars['vcf']) || ! is_author()) {
         return;
+    }
 
     // include custom template
-    include dirname( __FILE__ ) . '/vcf-template.php';
+    include dirname(__FILE__) . '/vcf-template.php';
     exit;
 }
-add_action( 'template_redirect', 'makeplugins_json_template_redirect' );
+add_action('template_redirect', 'makeplugins_json_template_redirect');
